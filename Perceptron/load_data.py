209895e26fb8418e9131f7e0a16602377e97021d
tj96 <tusharjandial@gmail.com>
2018-02-13 01:06:08 +0530

    -perceptron modified to multiclass perceptron -diferent datasets added in data folder

--- Perceptron/load_data.py
+++ Perceptron/load_data.py
@@ -6,15 +6,18 @@ def load_data(filename = "dataset.csv"):
 	np.random.shuffle(data)
 
 	data = data.astype(float)
-	traindata = data
 
-	n_rows = int(0.75*len(data))
+	# maxx = np.max(data,axis=0)
+	# minn = np.min(data,axis=0)
+	# data = (data - minn)/(maxx-minn)
+
+	n_rows = int(0.66*len(data))
 	n_cols = len(data[0])
 	rest_setx = data[:n_rows, :n_cols-1]
 	rest_sety = data[:n_rows, -1]
 	test_setx = data[n_rows:, :n_cols-1]
 	test_sety = data[n_rows:, -1]
-	return (rest_setx, rest_sety, test_setx, test_sety)
+	return rest_setx, rest_sety.astype(int), test_setx, test_sety.astype(int)
 
 def main():
 	filename = input()
--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -3,84 +3,50 @@ import load_data as ld
 import test
 import sys
 
-def normalize_data(A,mean,sd):              # normalizes and adds 1 in the begining of every row
-    l = []
-    for i in range(0,len(A)):
-        temp = [1]
-        for j in range(0,len(A[i])):
-            temp.append((A[i][j]-mean)/sd)
-        l.append(temp)
-
-    return np.array(l)
-
-def scale_classes(A):               # convert labels to -1 or 1
-    m = min(A)
-    l = []
-    for x in A:
-        if x == m:
-            l.append(-1)
-        else:
-            l.append(1)
-    return np.array(l)
-
-
-def initialize(d):
-        return np.random.normal(0, 1/np.sqrt(d), d)
-
-
-class Perceptron():
+class Model():
     def __init__(self, l_rate, batch_size, n_epoch):
         self.__lRate = l_rate
         self.__bSize = batch_size
         self.__epoch = n_epoch
-        self.__w = 0
+        self.__wList = 0
+        self.__nClass = 0
     ###################
 
     def train(self, X, Y):
         m = len(X)
         d = len(X[0])
+        n_class = len(set(Y))         # no. of classes
 
         l = self.__lRate
         n = self.__bSize
         e = self.__epoch
 
-        w = initialize(d)
+        w_list = [np.zeros(d)] * n_class
+
         for t in range(e):
-            error = np.array([0.0]*d)
-            count = 0
-            flag = True
-
             for x,y in zip(X,Y):
-                corr= x*y
-                if np.dot(w,corr) < 0.0:
-                    error -= corr
-                    flag = False
+                y1 = max([(np.dot(w_list[i],x),i) for i in range(n_class)])[1]
+                if y1 != y:
+                    w_list[y1] = w_list[y1] - x
+                    w_list[y] = w_list[y] + x
 
-                count += 1
-                if count == n:
-                    w += l * error/count
-                    count = 0
-                    error = np.array([0.0]*d)
-
-            if count > 0:
-                w += l * error/count
-
-            if flag:                # break if no error found in current epoch i.e early stopping
-                break
-
-        self.__w = w;
+        self.__wList = w_list;
+        self.__nClass = n_class
         print("Training Complete", t)
     ####################
 
     def test(self, x):
-        return np.sign(np.dot(self.__w, x))
+        return max([(np.dot(self.__wList[i],x),i) for i in range(self.__nClass)])[1]
 
 
 def getParameters():
-    print("Enter batch size (1 for online learning, 0 for full-batch):")
-    b= int(input())
-    if b == 0:
-        b = 99999999
+    # print("Enter batch size (1 for online learning, 0 for full-batch):")
+    b=1
+    l=0.1
+    e = 200
+    # b= int(input())
+    # if b == 0:
+    #     b = 99999999
 
     print("Enter learning rate:")
     l = float(input())
@@ -90,19 +56,10 @@ def getParameters():
 
     return l,b,e
 
-def main():
+def main(l,b,e):
     train_x, train_y, test_x, test_y = ld.load_data(sys.argv[1])
     
-    train_y = scale_classes(train_y)
-    test_y = scale_classes(test_y)
-    
-    mean = np.mean(train_x)
-    sd = np.std(train_y)
-    train_x = normalize_data(train_x,mean,sd)
-    test_x = normalize_data(test_x,mean,sd)     # test data is normalized using mean and sd of train data
-
-    b,l,e = getParameters()
-    model = Perceptron(b,l,e)
+    model = Model(l,b,e)
     model.train(train_x, train_y)
 
     output = []
@@ -110,6 +67,9 @@ def main():
         output.append((model.test(x) , y))
 
     print(test.accuracy(output))
+    return test.accuracy(output)
+
 
 if __name__ == '__main__':
-    main()
+    l,b,e = getParameters()
+    main(l,b,e)
